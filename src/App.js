ecd2aa3b0172d03dd2f30d245b0bfd13bd6eead4
mohinimraut <mohiniraut7585@gmail.com>
2022-05-31 23:04:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
+// import Header from './Header';
+// function App() {
+//   return (
+//     <div>
+//      <Header/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+import React from 'react'
+import Header from './Header';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+
+} from "react-router-dom";
+
+import Products from './Products';
+import AboutUS from './AboutUS';
+import ContactUS from './ContactUS';
+import Services from './Services';
+import Login from './Login';
+import SignUP from './SignUP'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+      <Header/>
+
+        <Routes>
+        <Route path="/products" element={<Products />}>
+          </Route>
+          <Route path="/aboutus" element={<AboutUS />}>
+          </Route>
+          <Route path="/contactus" element={<ContactUS />}>
+          </Route>
+
+         
+          <Route path="/services" element={<Services />}>
+          </Route>
+          
+          <Route path="/login" element={<Login />}>
+          </Route>
+          <Route path="/signup" element={<SignUP />}>
+          </Route>
+          {/* <Route path="/" element={<h1>Welcome</h1>}></Route> */}
+
+    
+          {/* <Route path='*' element={<h1>Page not Found</h1>}></Route> */}
+
+        </Routes>
+      </Router>
+
+    </>
+  )
 }
 
-export default App;
+export default App
